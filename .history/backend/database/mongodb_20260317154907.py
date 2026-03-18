ac507323cf87cc2b87d132032374f3862b62462a
Siva <sivakukkuluri82@gmail.com>
2026-03-18 19:27:52 +0530

    feat: finalized ASI-1 platform with full history and source

--- .history/backend/database/mongodb_20260317154907.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-# Hardcoding the local fallback to prevent InvalidURI errors during the hackathon
-MONGO_URL = "mongodb+srv://sivakukkuluri82:Siva5003@:@cluster0.fmzbynr.mongodb.net/?appName=Cluster0"
-client = AsyncIOMotorClient(MONGO_URL)
-
-db = client.threat_intelligence
-threats_collection = db.get_collection("threats")
-iot_devices_collection = db.get_collection("cloud_assets") # Changed from IoT
